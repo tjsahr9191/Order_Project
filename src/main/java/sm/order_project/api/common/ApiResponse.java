f8cd3e4604479fd00304ec61c61f546ca7f18a28
sunmok <rlfhr9191@gmail.com>
2025-08-15 17:54:25 +0900

    :sparkles: Make ApiResponse

---
+++ src/main/java/sm/order_project/api/common/ApiResponse.java
@@ -0,0 +1,45 @@
+package sm.order_project.api.common;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public class ApiResponse<T> {
+
+    private final int code;
+    private final HttpStatus status;
+    private final String message;
+    private final T data;
+
+    @Builder
+    private ApiResponse(HttpStatus status, String message, T data) {
+        this.code = status.value();
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+
+    public static <T> ApiResponse<T> of(HttpStatus httpStatus, String message, T data) {
+        return new ApiResponse<>(httpStatus, message, data);
+    }
+
+    public static <T> ApiResponse<T> of(HttpStatus httpStatus, T data) {
+        return of(httpStatus, httpStatus.name(), data);
+    }
+
+    public static <T> ApiResponse<T> of(HttpStatus httpStatus) {
+        return of(httpStatus, null);
+    }
+
+    public static <T> ApiResponse<T> ok(T data) {
+        return of(HttpStatus.OK, data);
+    }
+
+    public static <T> ApiResponse<T> create() {
+        return of(HttpStatus.CREATED);
+    }
+
+}
