89c13267d5ec529659bc816f7ab6c964e477c6ce
sunmok <rlfhr9191@gmail.com>
2025-12-06 22:51:25 +0900

    fix: TestRedisConfig

--- src/test/java/sm/order_project/config/TestRedisConfig.java
+++ src/test/java/sm/order_project/config/TestRedisConfig.java
@@ -1,38 +1,10 @@
 package sm.order_project.config;
 
-import jakarta.annotation.PreDestroy;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.junit.jupiter.Container;
 
-@TestConfiguration
+@TestConfiguration(proxyBeanMethods = false)
 public class TestRedisConfig {
-
-    @Container
-    private static final GenericContainer<?> redisContainer;
-
-    static {
-        redisContainer = new GenericContainer<>("redis:7.0")
-                .withExposedPorts(6379);
-        redisContainer.start();
-
-        String host = redisContainer.getHost();
-        Integer port = redisContainer.getMappedPort(6379);
-
-        System.setProperty("spring.data.redis.host", host);
-        System.setProperty("spring.data.redis.port", port.toString());
-    }
-
-    @Bean
-    public GenericContainer<?> redisContainer() {
-        return redisContainer;
-    }
-
-    @PreDestroy
-    public void stop() {
-        if (redisContainer.isRunning()) {
-            redisContainer.stop();
-        }
-    }
+    // Redis configuration is now handled by application-test.yml
+    // No additional beans needed - Spring Boot auto-configuration will use
+    // spring.data.redis.host and spring.data.redis.port from properties
 }
